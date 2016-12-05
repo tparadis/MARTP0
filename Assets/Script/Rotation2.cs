365c18206ee35ce9b19a94caa6b0bbaa369c7b7e
tparadis <theophile.paradis@gmail.com>
2016-12-05 10:26:39 +0100

    first commit

---
+++ Assets/Script/Rotation2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.left);
+		transform.Rotate (Vector3.down);
+
+	}
+}
